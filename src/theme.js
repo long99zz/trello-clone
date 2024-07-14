471d3cb1b1767445e1b2e40e03cf1e6d8bf34157
long99zz <zzdragon14@gmail.com>
2024-07-14 17:08:34 +0700

    design layout 1

---
+++ src/theme.js
@@ -0,0 +1,26 @@
+import { createTheme } from '@mui/material/styles'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+import { teal, cyan, deepOrange, orange } from '@mui/material/colors'
+
+const theme = extendTheme({
+    trello: {
+        appBarHeight: '48px',
+        boardBarHeight: '58px'
+    },
+    colorSchemes: {
+        light: {
+            palette: {
+                primary: teal,
+                secondary: deepOrange
+            }
+        },
+        dark: {
+            palette: {
+                primary: cyan,
+                secondary: orange
+            }
+        }
+    }
+})
+
+export default theme
