b6904313faea38b0ed155b447917d24443b3c6dd
long99zz <zzdragon14@gmail.com>
2024-07-18 11:00:19 +0700

    Continue design layout

---
+++ src/components/AppBars/Menus/Profiles.jsx
@@ -0,0 +1,80 @@
+import Tooltip from '@mui/material/Tooltip'
+import React from 'react'
+import Box from '@mui/material/Box'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Divider from '@mui/material/Divider'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import Avatar from '@mui/material/Avatar'
+import { IconButton } from '@mui/material'
+import PersonAdd from '@mui/icons-material/PersonAdd'
+import Settings from '@mui/icons-material/Settings'
+import Logout from '@mui/icons-material/Logout'
+import { Height } from '@mui/icons-material'
+
+function Profiles() {
+    const [anchorEl, setAnchorEl] = React.useState(null)
+    const open = Boolean(anchorEl)
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget)
+    }
+    const handleClose = () => {
+        setAnchorEl(null)
+    }
+    return (
+        <Box>
+            <Tooltip title="Account settings">
+                <IconButton
+                    onClick={handleClick}
+                    size="small"
+                    sx={{ padding: 0 }}
+                    aria-controls={open ? 'basic-menu-profiles' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                >
+                    <Avatar sx={{ width: 30, height: 30 }}
+                        alt='LongBa'
+                        src='https://images-ext-1.discordapp.net/external/qOpjIdoji3HhLLYQVFcFbr0Z9OIIwFNWJirH99_wIo8/https/pbs.twimg.com/media/GSrfo9IaYAAQ8J-.png%3Alarge?format=webp&width=676&height=676'
+                    />
+                </IconButton>
+            </Tooltip>
+            <Menu
+                id="basic-menu-profiles"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'basic-button-profiles',
+                }}
+            >
+                <MenuItem >
+                    <Avatar sx={{ width: 28, height: 28, mr: 2 }} /> Profile
+                </MenuItem>
+                <MenuItem >
+                    <Avatar sx={{ width: 28, height: 28, mr: 2 }} /> My account
+                </MenuItem>
+                <Divider />
+                <MenuItem >
+                    <ListItemIcon>
+                        <PersonAdd fontSize="small" />
+                    </ListItemIcon>
+                    Add another account
+                </MenuItem>
+                <MenuItem >
+                    <ListItemIcon>
+                        <Settings fontSize="small" />
+                    </ListItemIcon>
+                    Settings
+                </MenuItem>
+                <MenuItem >
+                    <ListItemIcon>
+                        <Logout fontSize="small" />
+                    </ListItemIcon>
+                    Logout
+                </MenuItem>
+            </Menu>
+        </Box>
+    )
+}
+
+export default Profiles
