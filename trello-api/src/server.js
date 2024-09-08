6499f985391fd45c7fa50870d148800ea066eceb
long99zz <zzdragon14@gmail.com>
2024-09-08 12:00:10 +0700

    create Node project & set environment

--- trello-api/src/server.js
+++ trello-api/src/server.js
@@ -1,13 +1,35 @@
-const express = require('express')
+import express from 'express'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import AsyncExitHook from 'async-exit-hook'
+import 'dotenv/config'
+import { env } from '~/config/environment'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
-app.get('/', function (req, res) {
-    res.send('<h1>Hello world</h1>')
-})
+  app.get('/', async (req, res) => {
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.listen(port, hostname, () => {
-    console.log(`Hello, I'm running server at http://${hostname}:${port}/`)
-})
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`Hello ${env.AUTHOR}, I am running at ${env.APP_HOST}:${env.APP_PORT}/`)
+  })
+
+  AsyncExitHook(() => {
+    CLOSE_DB()
+  })
+}
+
+const START_APP = async () => {
+  try {
+    console.log('Connecting to MongoDB Atlas...')
+    await CONNECT_DB()
+    console.log('Connected to MongoDB Atlas!!')
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+}
+
+START_APP()
