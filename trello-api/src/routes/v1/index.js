deabe174366f98ebda73889a6aef607d3d3c7037
long99zz <zzdragon14@gmail.com>
2024-09-15 10:31:04 +0700

    set api

--- trello-api/src/routes/v1/index.js
+++ trello-api/src/routes/v1/index.js
@@ -1,5 +1,8 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import express from 'express'
+const router = express.Router()
+
+router.get('/status', (req, res) => {
+    res.status(200).json({ message: 'APIs V1 are ready to use' })
+})
+
+export const APIs_V1 = router
