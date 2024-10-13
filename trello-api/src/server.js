20cf73036d777217e3c682563333150b1015ec48
long99zz <zzdragon14@gmail.com>
2024-10-13 22:08:57 +0700

    update env

--- trello-api/src/server.js
+++ trello-api/src/server.js
@@ -18,9 +18,16 @@ const START_SERVER = () => {
 
   app.use(errorHandlingMiddleware)
 
-  app.listen(env.APP_PORT, env.APP_HOST, () => {
-    console.log(`Hello ${env.AUTHOR}, I am running at ${env.APP_HOST}:${env.APP_PORT}`)
-  })
+  if (env.BUILD_MODE === 'production') {
+    app.listen(process.env.PORT, () => {
+      console.log(`Production: Hello ${env.AUTHOR}, I am running at Port:${process.env.PORT}`)
+    })
+  } else {
+    app.listen(env.APP_PORT, env.APP_HOST, () => {
+      console.log(`Local Dev: Hello ${env.AUTHOR}, I am running at ${env.APP_HOST}:${env.APP_PORT}`)
+    })
+  }
+
 
   AsyncExitHook(() => {
     CLOSE_DB()
